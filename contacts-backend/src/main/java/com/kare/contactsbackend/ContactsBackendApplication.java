cd845e3a17b84c49226f195f03eecebc04667cb6
Károly András Kovács <karoly0302@gmail.com>
2024-09-27 13:32:45 +0200

    init(): init backend for contacts app

---
+++ contacts-backend/src/main/java/com/kare/contactsbackend/ContactsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kare.contactsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ContactsBackendApplication.class, args);
+  }
+
+}
